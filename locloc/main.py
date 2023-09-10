cbceaefa5888423cd3d0922b783449129770a3b1
eggplants <w10776e8w@yahoo.co.jp>
2023-09-11 07:28:52 +0900

    add server

--- locloc/main.py
+++ locloc/main.py
@@ -1,7 +1,26 @@
+from . import __version__
 from .util import get_loc_stats
+from fastapi import FastAPI, Request
+import uvicorn
+from pathlib import Path
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory=Path(__file__).parent / "templates")
+
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "version": __version__})
 
 def main() -> None:
-    print(get_loc_stats("https://github.com/eggplants/getjump", "master"))
+    config = uvicorn.Config(app, port=5000, log_level="info")
+    server = uvicorn.Server(config)
+    server.run()
 
 if __name__ == "__main__":
     main()
